6a5557856bc642b44d1b3f311e54d7bc1cc97929
Luis Moyano <moyano@hey.com>
2026-01-24 13:54:12 +0100

    Testing gems one by one

--- ruby-tests/run.sh
+++ ruby-tests/run.sh
@@ -1,19 +1,19 @@
 gem uninstall shiny_json_logic json_logic_ruby json-logic-rb json_logic
 gem install shiny_json_logic
-export LIBRARY="shiny_json_logic_ruby"
+export LIBRARY="shiny_json_logic"
 ruby main.rb
 
-gem uninstall shiny_json_logic json_logic_ruby json-logic-rb json_logic
-gem install json_logic_ruby
-export LIBRARY="json_logic_ruby"
-ruby main.rb
-
-gem uninstall shiny_json_logic_ruby json_logic_ruby json-logic-rb json_logic
-gem install json-logic-rb
-export LIBRARY="json-logic-rb"
-ruby main.rb
-
-gem uninstall shiny_json_logic_ruby json_logic_ruby json-logic-rb json_logic
-gem install json_logic
-export LIBRARY="json_logic"
-ruby main.rb
+# gem uninstall shiny_json_logic json_logic_ruby json-logic-rb json_logic
+# gem install json_logic_ruby
+# export LIBRARY="json_logic_ruby"
+# ruby main.rb
+#
+# gem uninstall shiny_json_logic_ruby json_logic_ruby json-logic-rb json_logic
+# gem install json-logic-rb
+# export LIBRARY="json-logic-rb"
+# ruby main.rb
+#
+# gem uninstall shiny_json_logic_ruby json_logic_ruby json-logic-rb json_logic
+# gem install json_logic
+# export LIBRARY="json_logic"
+# ruby main.rb
